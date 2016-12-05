e42d20c2cc1ca6308d22564d207f264ec2cbac91
Reo Hokazono <reohokazono@gmail.com>
2016-12-05 23:35:29 +0900

    Initial Commit

---
+++ TrueScale/TrueScale.h
@@ -0,0 +1,19 @@
+//
+//  TrueScale.h
+//  TrueScale
+//
+//  Created by Reo Hokazono on 2016/12/05.
+//  Copyright © 2016年 Reo Hokazono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrueScale.
+FOUNDATION_EXPORT double TrueScaleVersionNumber;
+
+//! Project version string for TrueScale.
+FOUNDATION_EXPORT const unsigned char TrueScaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrueScale/PublicHeader.h>
+
+
